946dac38cc93cbc98214cf61912419836e819fca
erick485 <xtr3msoft@gmail.com>
2014-10-29 13:14:51 +0100

    Agregando css y js

--- app/views/platanitos/home.blade.php
+++ app/views/platanitos/home.blade.php
@@ -2,39 +2,39 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/semantic.css">
-  <link rel="stylesheet" type="text/css" href="css/home.css">
+  <link rel="stylesheet" type="text/css" href="css/semantic.min.css">
+
   <script src="js/jquery.js"></script>
   <script src="js/semantic.js"></script>
-  <script src="js/home.js"></script>
+  <script src="js/menu.js"></script>
 </head>
-<body id="home">
-  <div class="ui teal small menu">
-  <a class="active item">
-    <i class="home icon"></i> Home
-  </a>
-  <a class="item">
-    <i class="mail icon"></i> Messages
-  </a>1
-  <div class="right menu">
-    <div class="item">
-      <div class="ui icon input">
-        <input type="text" placeholder="Search...">
-        <i class="search link icon"></i>
-      </div>
-    </div>
-    <div class="ui dropdown item">
-      Language <i class="dropdown icon"></i>
-      <div class="menu">
-        <a class="item">English</a>
-        <a class="item">Russian</a>
-        <a class="item">Spanish</a>
-      </div>
-    </div>
-    <div class="item">
-        <div class="ui teal button">Sign Up</div>
-    </div>
-  </div>
-</div>
+<body>
+ <div class="ui pointing menu">
+   <a class="active item">
+     <i class="home icon"></i> Home
+   </a>
+   <a class="item">
+     <i class="mail icon"></i> Messages
+   </a>
+   <a class="item">
+     <i class="user icon"></i> Friends
+   </a>
+   <div class="right menu">
+     <div class="item">
+       <div class="ui icon input">
+         <input type="text" placeholder="Search...">
+         <i class="search link icon"></i>
+       </div>
+     </div>
+     <div class="ui dropdown item">
+       Language <i class="dropdown icon"></i>
+       <div class="menu">
+         <a class="item">English</a>
+         <a class="item">Russian</a>
+         <a class="item">Spanish</a>
+       </div>
+     </div>
+   </div>
+ </div>
 </body>
 </html>
