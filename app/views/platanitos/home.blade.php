b17097e5fefbea7cfac2d093948e51d3f2987385
Geinner Tucto <geinertucto@gmail.com>
2014-10-28 19:37:40 -0500

    semantic-ui

--- app/views/platanitos/home.blade.php
+++ app/views/platanitos/home.blade.php
@@ -2,8 +2,39 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/semantic.css">
+  <link rel="stylesheet" type="text/css" href="css/home.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/semantic.js"></script>
+  <script src="js/home.js"></script>
 </head>
-<body>
-<h1>Hola Mundo</h1>
+<body id="home">
+  <div class="ui teal small menu">
+  <a class="active item">
+    <i class="home icon"></i> Home
+  </a>
+  <a class="item">
+    <i class="mail icon"></i> Messages
+  </a>1
+  <div class="right menu">
+    <div class="item">
+      <div class="ui icon input">
+        <input type="text" placeholder="Search...">
+        <i class="search link icon"></i>
+      </div>
+    </div>
+    <div class="ui dropdown item">
+      Language <i class="dropdown icon"></i>
+      <div class="menu">
+        <a class="item">English</a>
+        <a class="item">Russian</a>
+        <a class="item">Spanish</a>
+      </div>
+    </div>
+    <div class="item">
+        <div class="ui teal button">Sign Up</div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
